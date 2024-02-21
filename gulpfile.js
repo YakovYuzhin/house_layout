08e203fbf28b7507dd98e3ff0cb9c49add4d9a9d
Yakov <gytersad07@gmail.com>
2024-02-21 18:15:15 +0300

    04. Added Sass

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+// function defaultTask(cb) {
+//   // place code for your default task here
+//   cb();
+// }
+
+// exports.default = defaultTask
+
+var gulp = require("gulp");
+var sass = require("gulp-sass")(require("sass"));
+
+gulp.task("sass", function (cb) {
+  gulp
+    .src("src/*.scss")
+    .pipe(sass())
+    .pipe(
+      gulp.dest(function (f) {
+        return f.base;
+      })
+    );
+  cb();
+});
+
+gulp.task(
+  "default",
+  gulp.series("sass", function (cb) {
+    gulp.watch("src/*.scss", gulp.series("sass"));
+    cb();
+  })
+);
